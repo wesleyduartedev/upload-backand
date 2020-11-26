93d7426a3653c93f8052d4d8237d3d4536f1920c
Wesley Duarte <wesleyduarte@hotmail.com>
2020-11-25 21:40:51 -0300

    ADD ENV

--- src/db/database.js
+++ src/db/database.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+
 const db = mongoose.connect('mongodb+srv://wesleyduartedev:191095veralucia@cluster0.kapsf.mongodb.net/snitchtracker', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
